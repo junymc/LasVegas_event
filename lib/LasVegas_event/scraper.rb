aa235ac87105d74a814028d1beb7a8917006e5b3
Junyoung McArdle <juny.mcardle@gmail.com>
2019-06-14 20:03:58 +0000

    Done.

--- lib/LasVegas_event/scraper.rb
+++ lib/LasVegas_event/scraper.rb
@@ -18,13 +18,9 @@ class LasVegasEvent::Scraper
 
   def self.get_url(event)
   binding.pry
-    if Scraper.get_url(event) == nil
       event_page = Nokogiri::HTML(open(event.url))
       event_webpage = event_page.css("div.a").attr("href")
       event.event_webpage = event_webpage
-    else
-      Scraper.get_url(event)
-    end
   end
 
 end
